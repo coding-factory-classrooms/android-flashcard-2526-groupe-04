dc3e4e0c146f913b47eef750f2d47cac8d0e8d69
RocherSilencieux <164058459+RocherSilencieux@users.noreply.github.com>
2025-10-07 16:17:26 +0200

    init

--- app/src/main/java/com/example/flashcard/ResultStatsActivity.java
+++ app/src/main/java/com/example/flashcard/ResultStatsActivity.java
@@ -1,7 +1,6 @@
 package com.example.flashcard;
 
 import android.os.Bundle;
-import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,21 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-
-    public static final String TAG = "MainActivity";
+public class ResultStatsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_result_stats);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        Log.d(TAG, "Hello Flashcard");
     }
 }
