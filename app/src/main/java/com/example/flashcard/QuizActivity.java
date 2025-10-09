d60e39004db394a6954d5a7b2ad2c238c56c54bc
RocherSilencieux <164058459+RocherSilencieux@users.noreply.github.com>
2025-10-09 21:30:30 +0200

    bug fixes, feat:ready to implement the API

--- app/src/main/java/com/example/flashcard/QuizActivity.java
+++ app/src/main/java/com/example/flashcard/QuizActivity.java
@@ -36,32 +36,35 @@ public class QuizActivity extends AppCompatActivity {
             return insets;
 
         });
+
         boolean answered = false;
+        //int currentQuestionIndex = getIntent().getParcelableExtra("currentQuestionIndex");
         int currentQuestionIndex = 0;//a set dans le menu et a transferer a chaque itération de questions
-        Quiz.Question question1 = new Quiz.Question("vrai","prout",3,R.drawable.drapeau);
+        Quiz.Question question1 = new Quiz.Question("vrai", "prout", 3, R.drawable.drapeau);
         ArrayList<Quiz.Question> liste = new ArrayList<Quiz.Question>();
         liste.add(question1);
-        Quiz quiz = new Quiz(liste,1);
-        dynamicAnswers(quiz.questions.get(currentQuestionIndex));
-        MediaPlayer mediaPlayer = setup(quiz);
+        //Quiz quiz = getIntent().getParcelableExtra("Quiz"); <- quand tout sera relié
+        Quiz quiz = new Quiz(liste, 1);
+        Quiz.Question currentQuestion = quiz.questions.remove(getRandom(1,quiz.numberOfQuestions)-1);
+        dynamicAnswers(currentQuestion);
+        MediaPlayer mediaPlayer = setup(currentQuestion);
         Button play = findViewById(R.id.audioButton);
         play.setOnClickListener(view -> {
             mediaPlayer.start();
         });
         Button validButton = findViewById(R.id.validationButton);
-        if(!answered){
-            validButton.setOnClickListener(view ->{
-                checkResult(quiz.questions.get(currentQuestionIndex));
-                validButton.setText("Continuer");
-            });
-        }else{
-            validButton.setOnClickListener(view ->{
+        validButton.setOnClickListener(view -> {
+            if (!answered) {
+                checkResult(currentQuestion);
+                validButton.setText("Continuer"); //<-le button pour passer a la question suivante marche pas mais tout y est il manque juste la condition du if
+            } else {
                 Intent intent = new Intent(this, QuizActivity.class);
+                intent.putExtra("currentQuestionIndex", currentQuestionIndex + 1);
+                intent.putExtra("Quiz", quiz);
 
+                startActivity(intent);
+            }
             });
-        }
-
-
     }
     public static int getRandom(int min, int max) {
 
@@ -69,8 +72,7 @@ public class QuizActivity extends AppCompatActivity {
         int random = (int) ((range * Math.random()) + min);
         return random;
     }
-    public MediaPlayer setup(Quiz quiz){
-        Quiz.Question currentQuestion = quiz.questions.get(getRandom(1,quiz.numberOfQuestions)-1);
+    public MediaPlayer setup(Quiz.Question currentQuestion){
         final int resourceId = getResources().getIdentifier(currentQuestion.filePath,"raw",this.getPackageName());
         MediaPlayer mediaPlayer = MediaPlayer.create(this,resourceId);
         setupAnswers(currentQuestion);
@@ -87,19 +89,23 @@ public class QuizActivity extends AppCompatActivity {
         }
     }
     public void setupAnswers(Quiz.Question question){
+        ArrayList<String> currentAnswers = new ArrayList<String>();
         RadioGroup answerRadioGroup = findViewById(R.id.answerRadioGroup);
         int indexGoodAnswer = (int) getRandom(0,question.numberOfAnswers-1);
         for(int i = 0 ; i < question.numberOfAnswers;i++){
             if(i == indexGoodAnswer){
                 int id = answerRadioGroup.getChildAt(indexGoodAnswer).getId();
                 TextView view = findViewById(id);
+                currentAnswers.add(question.answer);
                 view.setText(question.answer);
             }else{
                 int id = answerRadioGroup.getChildAt(i).getId();
                 TextView view = findViewById(id);
-                view.setText("Mauvaise Réponse");
+                currentAnswers.add("Mauvaise Réponse");//<-
+                view.setText("Mauvaise Réponse"); //<- A REMPLACER PAR UNE MAUVAISE REPONSE ALEATOIRE qui n'est pas deja dans currentAnswers
             }
         }
+        //for(int i;i <
     }
 
     public void checkResult(Quiz.Question question){
