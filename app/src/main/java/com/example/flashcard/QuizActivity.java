ee08a0fe502c70548102a1949b4db7c8a09aa95d
RocherSilencieux <164058459+RocherSilencieux@users.noreply.github.com>
2025-10-09 10:58:20 +0200

    peer programming

--- app/src/main/java/com/example/flashcard/QuizActivity.java
+++ app/src/main/java/com/example/flashcard/QuizActivity.java
@@ -12,11 +12,13 @@ import androidx.core.view.WindowInsetsCompat;
 import android.media.MediaPlayer;
 import android.provider.MediaStore;
 import android.text.BoringLayout;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import java.util.Random;
 
 import java.util.ArrayList;
 
@@ -42,24 +44,33 @@ public class QuizActivity extends AppCompatActivity {
         ArrayList<Quiz.Question> liste = new ArrayList<Quiz.Question>();
         liste.add(question1);
         Quiz quiz = new Quiz(liste,1);
+        setupAnswers(quiz.questions.get(0));
 
 
         Button validButton = findViewById(R.id.validationButton);
         validButton.setOnClickListener(view ->{
+            Log.d("QuizActivity","test ");
             checkResult(quiz.questions.get(0));
         });
 
 
+    }
+    public void setupAnswers(Quiz.Question question){
+        RadioGroup answerRadioGroup = findViewById(R.id.answerRadioGroup);
+        int id = answerRadioGroup.getChildAt((int) (Math.random()*3)).getId();
+        TextView view = findViewById(id);
+        view.setText(question.answer);
     }
 
     public void checkResult(Quiz.Question question){
         RadioGroup radioGroup = findViewById(R.id.answerRadioGroup);
 
             int idChecked = radioGroup.getCheckedRadioButtonId();
+            Log.d("QuizActivity","caca");
             RadioButton answer = findViewById(radioGroup.getChildAt(idChecked).getId());
-            String text = answer.getText().toString();
-            TextView textView =findViewById(R.id.resultText);
-            if(text == question.answer){
+            String userAnswer = answer.getText().toString();
+            TextView textView = findViewById(R.id.resultText);
+            if(userAnswer.equals(question.answer)){
                 textView.setText("Bien joué");
                 textView.setVisibility(VISIBLE);
             }
